85c0b7861afc7d4b4e4fb7b07af391db6ab27fda
Tim Sandberg <tasandberg@gmail.com>
2015-01-07 12:08:59 -0800

    Add command recognition/validation

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -6,8 +6,15 @@
     this.chat = chat;
     this.$chatwindow = $("#chat-window");
     this.$chatForm = $('#chat-form');
+    this.user = "Anonymous Nagger";
+
 
     this.registerListeners();
+  };
+
+  ChatUI.prototype.commands = {
+    "nick" : function() { console.log('changenick'); },
+    "changeRoom" : function() { console.log('changeroom'); }
   };
 
   ChatUI.prototype.registerListeners = function() {
@@ -21,12 +28,41 @@
 
     ui.$chatForm.on("submit", function(e) {
       e.preventDefault();
-      var messageTxt = $('.input-lg').val();
-      if (messageTxt == "") return;
-
-      ui.chat.sendMessage(messageTxt);
-      $('.input-lg').val('');
+      ui.processInput();
     });
-  }
+  };
+
+  ChatUI.prototype.processInput = function() {
+    var messageTxt = $('.input-lg').val();
+    if (messageTxt == "") return;
+
+    if (this.isCommand(messageTxt)) {
+      this.processCommand(messageTxt);
+    } else {
+      this.chat.sendMessage(messageTxt);
+      $('.input-lg').val('');
+    }
+
+  };
+
+  ChatUI.prototype.processCommand = function(cmd) {
+    var command = cmd.split('/')[1].split(' ')[0];
+
+    if (this.isValidCommand(command)) {
+      this.commands[command]();
+    } else {
+      console.log('invalid command');
+    }
+  };
+
+  ChatUI.prototype.isCommand = function(str) {
+    return str[0] === "/";
+  };
+
+  ChatUI.prototype.isValidCommand = function(cmd) {
+    return this.commands.hasOwnProperty(cmd);
+  };
+
+
 
 })(this);
